d3955d7c3372dfc40407f87b3ef8da68513e0673
Attila <attila@p4d.co.uk>
2020-02-09 13:32:55 +0000

    working on my first plugin

---
+++ my-testplugin/testplugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       My First Plugin
+ * Plugin URI:        http://aws-staging.co.uk
+ * Description:       basic API plugin
+ * Version:           1.0.0
+ * Author:            Attila Balog
+ * Author URI:        http://aws-staging.co.uk
+ */
+
+if (!empty('ABSPATH')) {
+	exit;
+}
+echo 'hi there...';
